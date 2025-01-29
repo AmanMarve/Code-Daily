a67784c79e74efd47ec8c5d321ac521093f92dbd
Aman <aman1.marve@gmail.com>
2025-01-29 07:51:44 +0530

    v1.0.4 `Day 4 practices projects`

--- Day3(to-do list)/script.js
+++ Day3(to-do list)/script.js
@@ -7,32 +7,37 @@ function addTask() {
     //select value of input
     const taskText = taskInput.value;
     //created li 
-    const li = document.createElement("li");
-    // li ke andar input value
-    li.textContent = taskText;
+    if (taskText == '') {
+        alert("Kuch to likho")
+    } else {
+        const li = document.createElement("li");
+        // li ke andar input value
+        li.textContent = taskText;
 
-    //remove btn banayi uska code likha
-    const removeBtn = document.createElement("Button");
-    //inside text of btn
-    removeBtn.textContent = "remove";
-    //className diyaa
-    removeBtn.className = 'removeBtn';
-    //click pr ek arrow function chalaya
-    removeBtn.onclick = () => removeTask(li);
+        //remove btn banayi uska code likha
+        const removeBtn = document.createElement("Button");
+        //inside text of btn
+        removeBtn.textContent = "remove";
+        //className diyaa
+        removeBtn.className = 'removeBtn';
+        //click pr ek arrow function chalaya
+        removeBtn.onclick = () => removeTask(li);
 
-    //yaha se btn appear hui remove ki
-    li.appendChild(removeBtn);
+        //yaha se btn appear hui remove ki
+        li.appendChild(removeBtn);
 
-    //yaha se list appear hui
-    listContainer.appendChild(li)
+        //yaha se list appear hui
+        listContainer.appendChild(li)
 
-    //empty value set kr di input ki
-    taskInput.value = '';
+        //empty value set kr di input ki
+        taskInput.value = '';
 
-    //remove task btn ka function.
-    function removeTask() {
-        listContainer.removeChild(li);
+        //remove task btn ka function.
+        function removeTask() {
+            listContainer.removeChild(li);
+        }
     }
+
 
 }
 
